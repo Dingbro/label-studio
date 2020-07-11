c2d07e824f0421e9f2f42c7112aa62002cbcfb62
JaeYoung Jo <whwodud9@kaist.ac.kr>
2020-07-11 20:28:16 +0900

    Update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-label-studio start ${PROJECT_NAME:-my_project} --init -b --host 0.0.0.0 --port ${PORT:-8080}
+label-studio start ${PROJECT_NAME:-trash_project} --init -b --host 0.0.0.0 --port ${PORT:-8080} --username ${USERNAME:-dingbro} --password ${PASSWORD:-qwer1234!}
