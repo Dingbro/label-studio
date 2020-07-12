bf01cfc4c8ab50a51c13d0b6853bb05661fc0d94
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2020-07-12 14:29:12 +0900

    Update extension regex

--- tools/run.sh
+++ tools/run.sh
@@ -3,9 +3,10 @@
 label-studio start ${PROJECT_NAME:-trash_project} --init -b --host 0.0.0.0 --port ${PORT:-8080} \
 --username dingbro --password qwer1234 \
 --source s3 --source-path trash-image-dataset \
---source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images\", \"regex\": \"^.*.(jpeg|png|JPG|gif|GIF|doc|DOC|pdf|PDF)\" }" \
+--source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images\", \"regex\": \"^.*.(jpeg|jpg|png|JPG|PNG)\" }" \
 --target s3-completions --target-path trash-image-dataset \
 --target-params "{\"prefix\": \"output\", \"create_local_copy\": true}" \
 -l config.xml -c config.json \
---sampling uniform
+--sampling uniform \
+--log-level ERROR
 
