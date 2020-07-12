c5080138c62f309518ac8b63e5a331a47874df10
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2020-07-12 17:35:32 +0900

    Update output dir issue and limited to jpg and jpeg

--- tools/run.sh
+++ tools/run.sh
@@ -3,7 +3,7 @@
 label-studio start ${PROJECT_NAME:-trash_project} --init -b --host 0.0.0.0 --port 8082 \
 --username dingbro --password qwer1234 \
 --source s3 --source-path trash-image-dataset \
---source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images\", \"regex\": \"^.*.(jpeg|jpg|png|JPG|PNG)\" }" \
+--source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images\", \"regex\": \"^.*.(jpeg|jpg|JPG|JPEG)\" }" \
 --target s3-completions --target-path trash-image-dataset \
 --target-params "{\"prefix\": \"output\", \"create_local_copy\": true}" \
 -l config.xml -c config.json \
