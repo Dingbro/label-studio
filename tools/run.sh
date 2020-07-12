f5149bfc0b31a8f5db3ee90013a8cf156d96acf6
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2020-07-12 15:26:26 +0900

    Update port

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-label-studio start ${PROJECT_NAME:-trash_project} --init -b --host 0.0.0.0 --port ${PORT:-8080} \
+label-studio start ${PROJECT_NAME:-trash_project} --init -b --host 0.0.0.0 --port 8082 \
 --username dingbro --password qwer1234 \
 --source s3 --source-path trash-image-dataset \
 --source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images\", \"regex\": \"^.*.(jpeg|jpg|png|JPG|PNG)\" }" \
